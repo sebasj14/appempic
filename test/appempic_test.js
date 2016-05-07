24395313013504dbaf3c0a41e4c0603f4a9a89bb
Sebastián Jiménez <sebasj14@gmail.com>
2016-05-07 10:56:14 -0500

    Prueba para fallar test

--- test/appempic_test.js
+++ test/appempic_test.js
@@ -5,12 +5,12 @@ var assert = require('should');
 
 describe('appempic', function () {
 
-  it('El nombre debe ser Sebastián', function () {
-    appempic.getName().should.equal('Sebastián');
+  it('El nombre debe ser Sebastian', function () {
+    appempic.getName().should.equal('Sebastian');
   });
 
-  it('El apellido debe ser Sebastián', function () {
-    appempic.getLastName().should.equal('Jiménez');
+  it('El apellido debe ser Jimenez', function () {
+    appempic.getLastName().should.equal('Jimenez');
   });
 
 
